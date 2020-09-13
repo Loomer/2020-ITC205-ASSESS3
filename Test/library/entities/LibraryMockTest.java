6d34d2c63909c7458df3c3f8eb41116a71c106c5
Cezanne <cjalamgir@gmail.com>
2020-09-13 11:38:03 +1000

    Test for addPatron using Mocks

--- Test/library/entities/LibraryMockTest.java
+++ Test/library/entities/LibraryMockTest.java
@@ -1,6 +1,9 @@
 package library.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,12 +11,24 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import library.entities.helpers.IBookHelper;
+import library.entities.helpers.ILoanHelper;
+import library.entities.helpers.IPatronHelper;
 
 @ExtendWith(MockitoExtension.class)
 class LibraryMockTest {
-
+    
+    @Mock IBookHelper mockBookHelper;
+    @Mock IPatronHelper mockPatronHelper;
+    @Mock ILoanHelper mockLoanHelper;
+    
+    @Mock IPatron mockPatron;
+    
+    @InjectMocks
     Library library;
     
     @BeforeAll
@@ -26,6 +41,7 @@ class LibraryMockTest {
 
     @BeforeEach
     void setUp() throws Exception {
+        //patron =
     }
 
     @AfterEach
@@ -33,8 +49,24 @@ class LibraryMockTest {
     }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testLibraryAllParamsOK() {
+        
+        //arrange
+        String ln = "Smith";
+        String fn = "John";
+        String email = "jsmith@phaykmail.com";
+        long phNum = 1234567890;
+        int id = 1;
+        
+        when(mockPatronHelper.makePatron(ln, fn, email, phNum, id)).thenReturn(mockPatron);
+        
+        //act
+        IPatron patron = library.addPatron(ln, fn, email, phNum);
+        
+        // assert
+        assertNotNull(patron);
+        List<IPatron> plist = library.getPatronList();
+        assertTrue(plist.size() == 1);
     }
 
 }
