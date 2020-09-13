dd8ef33c39c937e0498032a6c3027f833bed29ee
Cezanne <cjalamgir@gmail.com>
2020-09-13 10:48:59 +1000

    Magic incantations

--- Test/library/entities/LibraryMockTest.java
+++ Test/library/entities/LibraryMockTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class LibraryMockTest {
 
     Library library;
